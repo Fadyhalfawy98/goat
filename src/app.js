ccfb772e98fe30421cc6b88533ca95aa21288aa9
Fady ElHalfawy <fadyhalfawy@gmail.com>
2021-12-28 19:03:08 +0200

    Add app.js.

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import {Component} from "react";
+
+class App extends Component {
+    render() {
+        return(
+            <button>
+                Test
+            </button>
+        )
+    }
+}
+
+export default App;
