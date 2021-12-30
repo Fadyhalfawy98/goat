eb34a60cdcbf7f0d6a389ad50a28741c75c8a26d
Fady ElHalfawy <fadyhalfawy@gmail.com>
2021-12-30 08:06:31 +0200

    Update navbar.jsx by making GOAT header.

--- src/components/common/navbar.jsx
+++ src/components/common/navbar.jsx
@@ -5,8 +5,8 @@ import React from "react";
 const NavBar = ({ user }) => {
 
     return(
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link className="navbar-brand" to="/">GOAT</Link>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <Link className="navbar-brand h1" to="/">GOAT </Link>
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div className="navbar-nav">
                     <NavLink className="nav-item nav-link" to="/home">
