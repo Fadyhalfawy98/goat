cc6991c2778eafe4aad539b06ee951e18e7d017d
Fady ElHalfawy <fadyhalfawy@gmail.com>
2021-12-30 06:29:23 +0200

    Add handleButtonClick.js.

---
+++ src/helperFunctions/handleButtonTransfer.js
@@ -0,0 +1,7 @@
+const HandleButtonTransfer = (history, path, label)  => {
+
+    if (label === "Signup" || label === "Login") return history.replace(path);
+
+    return history.push(path);
+}
+export default HandleButtonTransfer;
