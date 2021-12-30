1cc410dba96d86b3f428ea07a6cf09d5cdab7864
Fady ElHalfawy <fadyhalfawy@gmail.com>
2021-12-30 07:54:18 +0200

    Update app.js by adding ProtectedRoute and logout route.

--- src/app.js
+++ src/app.js
@@ -2,22 +2,40 @@ import React from "react";
 import {Component} from "react";
 import {Redirect, Route, Switch} from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import auth from "./services/authService";
+import NavBar from "./components/common/navbar";
+import ProtectedRoute from "./components/common/protectedRoute";
+import Logout from "./components/common/logout";
 import LoginForm from "./components/forms/loginForm";
 import SignupForm from "./components/forms/signupForm";
 import NotFoundForm from "./components/forms/notFoundForm";
+import Audit from "./components/forms/audit";
 
 class App extends Component {
+    state = {}
+
+    componentDidMount() {
+        const user = auth.getCurrentUser();
+        this.setState({ user });
+    }
+
     render() {
+        const { user } = this.state;
+
         return(
             <React.Fragment>
 
                 <ToastContainer />
+
+                <NavBar user={user}/>
 
                 <main className={"container"}>
 
                     <Switch>
                         <Route path={"/login"} component={LoginForm} />
                         <Route path={"/signup"} component={SignupForm} />
+                        <ProtectedRoute path={"/audit"} component={Audit} />
+                        <Route path={"/logout"} component={Logout} />
                         <Route path={"/notfound"} component={NotFoundForm} />
                         <Redirect from={"/"} exact to={"/login"} />
                         <Redirect to={"/notfound"} />
