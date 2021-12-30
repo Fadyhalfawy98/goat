4fa64a107839e2accabfba1a9db69ec0666808ec
Fady ElHalfawy <fadyhalfawy@gmail.com>
2021-12-30 07:23:47 +0200

    Update indext.tsx by adding comment for react-router-dom.

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// @ts-ignore
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
