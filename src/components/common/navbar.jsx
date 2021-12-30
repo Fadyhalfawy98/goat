2e6df734492ea2b98a4105cd5824160682bf8d0f
Fady ElHalfawy <fadyhalfawy@gmail.com>
2021-12-30 19:06:59 +0200

    Update navbar component by adding icon instead of label.

--- src/components/common/navbar.jsx
+++ src/components/common/navbar.jsx
@@ -3,10 +3,18 @@ import Link from "react-router-dom/Link";
 import React from "react";
 
 const NavBar = ({ user }) => {
+    const url = "https://thumbs.dreamstime.com/z/vector-goat-head-design-white-background-wild-animals-vector-goat-head-design-white-background-wild-animals-101979508.jpg";
 
     return(
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <Link className="navbar-brand h1" to="/">GOAT </Link>
+            <Link className="navbar-brand" to="/">
+                <img
+                    src={url}
+                    width="75"
+                    height="75"
+                    className="d-inline-block align-top mr-3"
+                    alt="No Image Found"/>
+            </Link>
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div className="navbar-nav">
                     <NavLink className="nav-item nav-link" to="/home">
