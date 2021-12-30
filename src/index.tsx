f37c519f153ba636b4479c5285fa30111d6d26ec
Fady ElHalfawy <fadyhalfawy@gmail.com>
2021-12-30 06:30:31 +0200

    Update index.tsx.

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
 import './index.css';
 import App from "./app";
+import logger from "./services/loggerService";
 import reportWebVitals from './reportWebVitals';
 
+logger.init();
+
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
