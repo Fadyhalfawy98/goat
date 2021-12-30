96765fb4b916ad235f2374cf6871b31c2a40cbc8
Fady ElHalfawy <fadyhalfawy@gmail.com>
2021-12-30 06:29:40 +0200

    Update app.js.

--- src/app.js
+++ src/app.js
@@ -1,12 +1,23 @@
 import {Component} from "react";
+import React from "react";
+import { ToastContainer } from "react-toastify";
+import LoginForm from "./components/forms/loginForm";
 
 class App extends Component {
     render() {
         return(
-            <button>
-                Test
-            </button>
-        )
+            <React.Fragment>
+
+                <ToastContainer />
+
+                <main className={"container"}>
+
+                    <LoginForm />
+
+                </main>
+
+            </React.Fragment>
+        );
     }
 }
 
