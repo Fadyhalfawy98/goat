308c10ed25761cf2988936d4f26ef730023f6338
Fady ElHalfawy <fadyhalfawy@gmail.com>
2022-01-03 04:08:17 +0200

    Add roomService.js to services.

---
+++ src/services/roomService.js
@@ -0,0 +1,51 @@
+const rooms = [
+    {
+        _id: "1",
+        title: "Room1",
+        genre: { _id: "0", name: "Free" }
+    },
+    {
+        _id: "2",
+        title: "Room2",
+        genre: { _id: "0", name: "Free" }
+    },
+    {
+        _id: "3",
+        title: "Room3",
+        genre: { _id: "0", name: "Free" }
+    },
+    {
+        _id: "4",
+        title: "Room4",
+        genre: { _id: "0", name: "Free" }
+    },
+    {
+        _id: "5",
+        title: "Room5",
+        genre: { _id: "0", name: "Free" }
+    },
+    {
+        _id: "6",
+        title: "Room6",
+        genre: { _id: "0", name: "Free" }
+    },
+    {
+        _id: "7",
+        title: "Room7",
+        genre: { _id: "0", name: "Free" }
+    },
+    {
+        _id: "8",
+        title: "Room8",
+        genre: { _id: "0", name: "Free" }
+    },
+    {
+        _id: "9",
+        title: "Room9",
+        genre: { _id: "0", name: "Free" }
+    }
+];
+
+export function getRooms() {
+    return rooms;
+}
